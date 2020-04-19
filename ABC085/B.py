1515479411c4201fd64745e50c8ff66e66cd1020
takumiw <w.tak.1229@gmail.com>
2020-04-19 14:25:10 +0900

    Add: ABC085/{A-C}.py

--- ABC085/B.py
+++ ABC085/B.py
@@ -1,3 +1,13 @@
-N = int(input())
-D = [int(input()) for _ in range(N)]
-print(len(set(D)))
+import sys, math
+sys.setrecursionlimit(10 ** 6)
+from collections import deque, defaultdict, Counter
+from operator import itemgetter
+
+def main():
+    N = int(input())
+    D = [int(input()) for _ in range(N)]
+    print(len(set(D)))
+
+
+if __name__ == '__main__':
+    main()
--- ABC085/C.py
+++ ABC085/C.py
@@ -1,13 +1,17 @@
-N, Y = map(int, input().split())
+import sys
 
 def main():
-    for num_10000 in range(N+1):
-        for num_5000 in range(N+1-num_10000):
-            num_1000 = N - num_10000 - num_5000
-            if num_10000*10000 + num_5000*5000 + num_1000*1000 == Y:
-                print(num_10000, num_5000, num_1000)
-                return
-    print('-1 -1 -1')
+    N, Y = map(int, sys.stdin.readline().rstrip().split())
 
-if __name__ == "__main__":
+    for n in range(Y//10000+1):
+        for m in range((Y-n*10000)//5000+1):
+            l = N - n - m
+            if l >= 0 and n*10000 + m*5000 + l*1000 == Y:
+                print(n, m, l)
+                return
+    
+    print(-1, -1, -1)
+
+
+if __name__ == '__main__':
     main()

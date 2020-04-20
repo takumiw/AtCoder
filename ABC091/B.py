fc7d6374e5606b7d4ee3cf1befa15dfbc2683d44
takumiw <w.tak.1229@gmail.com>
2020-04-20 17:10:51 +0900

    Add: ABC091/{A-B}.py

---
+++ ABC091/B.py
@@ -0,0 +1,10 @@
+N = int(input())
+S = [input() for _ in range(N)]
+M = int(input())
+T = [input() for _ in range(M)]
+
+ans = 0
+for s in set(S):
+    ans = max(ans, S.count(s) - T.count(s))
+
+print(ans)

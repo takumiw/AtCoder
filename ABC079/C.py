24606d4adabd37d6f0ceedad05354a7ba571b717
takumiw <w.tak.1229@gmail.com>
2020-04-20 17:26:25 +0900

    Add: ABC079/{A-C}.py

--- ABC079/C.py
+++ ABC079/C.py
@@ -1,10 +1,17 @@
-numbers = list(input())
-pm = ['+', '-']
+import sys
+readline = sys.stdin.readline
 
-for op1 in pm:
-    for op2 in pm:
-        for op3 in pm:
-            s = numbers[0]+op1+numbers[1]+op2+numbers[2]+op3+numbers[3]
-            if eval(s) == 7:
-                print('{}=7'.format(s))
-                exit()
+def main():
+    num = list(readline())
+    op = ['+', '-']
+    for op1 in op:
+        for op2 in op:
+            for op3 in op:
+                f = num[0] + op1 + num[1] + op2 + num[2] + op3 + num[3]
+                if eval(f) == 7:
+                    print(f + '=7')
+                    return
+
+
+if __name__ == '__main__':
+    main()

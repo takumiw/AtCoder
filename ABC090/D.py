3bee25823f61f0efc798bbd37d67ca793022c389
takumiw <w.tak.1229@gmail.com>
2020-05-08 17:36:27 +0900

    Add: ABC090/D.py

---
+++ ABC090/D.py
@@ -0,0 +1,19 @@
+import sys
+readline = sys.stdin.readline
+
+def main():
+    N, K = map(int, readline().rstrip().split())
+    if K == 0:
+        print(N ** 2)
+        return
+    ans = 0
+    for b in range(K, N+1):
+        r = (N+1) % b
+        p = (N+1) // b
+        ans += (b-K) * p + max(0, r - K)
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()

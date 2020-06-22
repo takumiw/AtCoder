4f56699affcad7352a3dafe28aa12421b5303130
takumiw <w.tak.1229@gmail.com>
2020-06-22 13:01:00 +0900

    Update: ChokudaiSpeedRun001/H.py

--- ChokudaiSpeedRun001/H.py
+++ ChokudaiSpeedRun001/H.py
@@ -1,22 +1,23 @@
 import sys
 readline = sys.stdin.readline
 from bisect import bisect_left
-INF = 10 ** 10
+INF = 10 ** 6
 
 
 def main():
     N = int(readline())
     A = list(map(int, readline().rstrip().split()))
-    dp = [INF for _ in range(N)]
+    dp = [INF] * (N + 1)
+    dp[0] = 0
     res = 0
-    
+
     for a in A:
         i = bisect_left(dp, a)
         dp[i] = a
         res = max(res, i)
     
-    print(res + 1)
+    print(res)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
